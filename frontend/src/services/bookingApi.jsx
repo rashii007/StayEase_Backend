103621734c39f69b8e09bb63e2ee961d90100358
Muhammad Rashid Khan <pmik754@gmail.com>
2026-08-03 23:45:12 +0100

    Add Vercel SPA rewrite

--- frontend/src/services/bookingApi.jsx
+++ frontend/src/services/bookingApi.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+console.log("API_BASE_URL:", API_BASE_URL);
 export const getBookings = async () => {
   try {
     const response = await axios.get(`${API_BASE_URL}/bookings`);
--- frontend/src/services/roomApi.jsx
+++ frontend/src/services/roomApi.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_URL;
+  console.log("API URL:", API_BASE_URL);
 export const getRooms = async () => {
   try {
     const response = await axios.get(`${API_BASE_URL}/rooms`);
