6d3aa763b1997ddc022baa2f78846dcc5db38846
Muhammad Rashid Khan <pmik754@gmail.com>
2026-08-04 12:36:36 +0100

    Update AuthApi

--- frontend/src/services/roomApi.jsx
+++ frontend/src/services/roomApi.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL;
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
   console.log("API URL:", API_BASE_URL || "http://localhost:5000/api");
 export const getRooms = async () => {
   try {
